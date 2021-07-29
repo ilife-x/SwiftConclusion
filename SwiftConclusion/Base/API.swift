3065779c277856a408be69e7a3bd76270599aea0
xiao <ilife_x@163.com>
2021-07-29 21:52:12 +0800

    add table

---
+++ SwiftConclusion/Base/API.swift
@@ -0,0 +1,12 @@
+//
+//  API.swift
+//  SwiftConclusion
+//
+//  Created by xiao on 2021/7/29.
+//
+
+import Foundation
+struct API {
+    static let imgrank = "http://m2.qiushibaike.com/article/list/imgrank"
+
+}
--- SwiftConclusion/Setting/SettingViewController.swift
+++ SwiftConclusion/Setting/SettingViewController.swift
@@ -6,13 +6,73 @@
 //
 
 import UIKit
+import Alamofire
 
 class SettingViewController: UIViewController {
 
+    lazy var tableview = UITableView()
+    static let cellID:String = "cellID"
+    
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        configUI()
+        loadData()
+
+    }
+    
+    
+    func configUI() {
+        tableview.delegate = self
+        tableview.dataSource = self
+        view.addSubview(tableview)
+        tableview.snp.makeConstraints { make in
+            make.top.left.right.bottom.equalTo(0)
+        }
+    }
+    
+    func loadData() {
+        
+        struct Page: Encodable {
+            let page: Int
+        }
+
+        let page = Page(page: 1)
+
+        AF.request(API.imgrank,
+                   method:.post,
+                   parameters: page,
+                   encoder: JSONParameterEncoder.default).response { response in
+            debugPrint(response)
+        }
+        
+        
+        
+        
 
     }
 
 
+}
+
+//MARK:- delegate
+extension SettingViewController:UITableViewDelegate{
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableview.dequeueReusableCell(withIdentifier: Self.cellID)
+        if cell  == nil {
+            cell = UITableViewCell(style: .default, reuseIdentifier: Self.cellID)
+        }
+        cell?.selectionStyle = .none
+        
+        return cell!
+
+    }
+}
+
+extension SettingViewController:UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        100
+    }
 }
